daa2fbd398aadc8918e16e8d13ec12e9dc3e9d0b
Leonardo Iribarne Paiva Magalhães <leo20042010@hotmail.com>
2025-10-30 22:32:50 -0300

    Listamos os nomes dos restaurantes na tela home e exibimos um slider

--- lib/tela_home.dart
+++ lib/tela_home.dart
@@ -1,8 +1,12 @@
 
 
 import 'package:aula1/banco/usuarioDAO.dart';
+import 'package:aula1/restaurante.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+
+import 'banco/resturanteDAO.dart';
 
 class telaHome extends StatefulWidget{
   @override
@@ -11,6 +15,22 @@ class telaHome extends StatefulWidget{
 
 class telaHomeState extends State<telaHome>{
   final nome = UsuarioDAO.usuarioLogado.nome;
+
+  List<Restaurante>  restaurantes = [];
+
+  Future<void> carregarRestaurantes() async{
+    final lista = await RestauranteDAO.listarTodos();
+    setState(() {
+      restaurantes = lista;
+    });
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    carregarRestaurantes();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +42,33 @@ class telaHomeState extends State<telaHome>{
 
         ],
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            SizedBox(
+                width: double.infinity,
+                height: 150,
+                child: ImagensSlider()
+            ),
+            SizedBox(
+              height: 55,
+              width: double.infinity,
+              child:ListView.builder(
+                  itemCount: restaurantes.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index){
+                    final r = restaurantes[index];
+                    return ElevatedButton(
+                        onPressed: (){},
+                        child: Text(r.nome ?? 'Sem nome')
+                    );
+                  }
+              ),
+            ),
+          ],
+        ),
+      ),
       bottomNavigationBar: BottomNavigationBar(items: <BottomNavigationBarItem>[
         BottomNavigationBarItem(icon: Icon(Icons.home), label: 'carrinho'),
         BottomNavigationBarItem(icon: Icon(Icons.savings_rounded), label: 'descontos'),
@@ -29,6 +76,20 @@ class telaHomeState extends State<telaHome>{
       ]),
     );
   }
-  
-  
+}
+
+class ImagensSlider extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+     return ListView(
+       scrollDirection: Axis.horizontal,
+       children: [
+         for (final color in Colors.primaries)
+           Container(width: 160, color: color),
+       ],
+     );
+  }
+
+
 }
